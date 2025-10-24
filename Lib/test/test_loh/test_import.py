bc3eb582c38d88b0da913097c4812774dd058aaf
Rob Barnes <robertcbarnes@gmail.com>
2025-10-24 08:21:59 -0600

    Improve test_import.py

--- Lib/test/test_loh/test_import.py
+++ Lib/test/test_loh/test_import.py
@@ -1,5 +1,7 @@
 import unittest
 
+from itertools import *
+
 class Tests(unittest.TestCase):
     def test_import(self):
         /math
@@ -8,6 +10,47 @@ class Tests(unittest.TestCase):
     def test_from(self):
         /math/sqrt
         assert sqrt(4) == 2
+
+    def test_as(self):
+        /math => m
+        assert m.sqrt(4) == 2
+
+        /math/sqrt => s
+        assert s(4) == 2
     
+    def test_import_all(self):
+        #intertools imported above at module level
+        assert isinstance(chain(), chain)
+
+    def test_import_multiple(self):
+        / math / sqrt, floor
+        assert sqrt(4) == 2
+        assert floor(4.5) == 4
+
+        / math, datetime
+        assert sqrt(4) == 2 
+        assert datetime.datetime.now()
+
+        / os / (path, listdir)
+        assert path
+        assert listdir
+
+    def test_import_multiple_as(self):
+        / math => m, datetime => d
+        assert m.sqrt(4) == 2 
+        assert d.datetime.now()
+
+    def test_import_submodule(self):
+        / xml.etree.ElementTree
+        assert xml.etree.ElementTree
+
+        / xml.etree / ElementTree
+        assert ElementTree
+
+        / xml.etree / ElementTree => ET
+        assert ET
+
+    def test_relative_import(self):
+        pass
     
     
